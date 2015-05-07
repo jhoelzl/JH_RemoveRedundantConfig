fa8fef264cedbb9265e2f4ee3e81bcc5728e6e5c
Josef Hölzl <josef.hoelzl@anton-paar.com>
2015-05-07 15:31:04 +0200

    Fixed typo

--- app/code/community/JH/RemoveRedundantConfig/Test/Config/ActivationXml.php
+++ app/code/community/JH/RemoveRedundantConfig/Test/Config/ActivationXml.php
@@ -1,6 +1,6 @@
 <?php
 
-class JH_RemoveRedundantConfig_Test_Config_ActivationXml extends EcomDev_PHPUnit_Test-Case_Config
+class JH_RemoveRedundantConfig_Test_Config_ActivationXml extends EcomDev_PHPUnit_Test_Case_Config
 {
 	
 	public function codePool()
