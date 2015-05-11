6db1c5316bbb6606f66d96ee4a5ee61f59a74247
Josef Hölzl <josef.hoelzl@anton-paar.com>
2015-05-11 14:34:41 +0200

    Test Update

--- app/code/community/JH/RemoveRedundantConfig/Test/Model/Observer.php
+++ app/code/community/JH/RemoveRedundantConfig/Test/Model/Observer.php
@@ -2,18 +2,26 @@
 
 class JH_RemoveRedundantConfig_Test_Model_Observer extends EcomDev_PHPUnit_Test_Case_Config
 {
-	
+	 /**
+     * @test
+     */
+     
 	public function removeSettingsCron()
 	{
 		$this->assertModuleCodePool('community');	
 	}
 	
+	 /**
+     * @test
+     */
 	public function removeSettings()
 	{
 		$this->assertModuleCodePool('community');	
 	}
 	
-	
+	 /**
+     * @test
+     */
 	public function findSettings()
 	{
 		
